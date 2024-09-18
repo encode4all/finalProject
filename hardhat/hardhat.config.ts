83b6cc4dfe1525bf785bc33660cc864ab2fecf3a
Idiakose O. Sunday <idiakosesunday@gmail.com>
2024-09-18 07:28:30 +0100

    chore: install eslint and prettier for dev

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -2,15 +2,17 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
 
 import dotenv from "dotenv";
-dotenv.config({ path: '../.env' });
+dotenv.config({ path: "../.env" });
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
   networks: {
     sepolia: {
-      url: process.env.RPC_ENDPOINT || "https://ethereum-sepolia-rpc.publicnode.com",
+      url:
+        process.env.RPC_ENDPOINT ||
+        "https://ethereum-sepolia-rpc.publicnode.com",
       // accounts: [`0x${process.env.PRIVATE_KEY}`]
-    }
+    },
   },
 };
 
--- hardhat/test/TestCommitReveal.ts
+++ hardhat/test/TestCommitReveal.ts
@@ -4,23 +4,23 @@ import { viem } from "hardhat";
 import { hexToString } from "viem";
 
 async function deployContractFixture() {
-    const publicClient = await viem.getPublicClient();
-    const [owner, otherAccount] = await viem.getWalletClients();
-    const contract = await viem.deployContract("CommitAndReveal", [])
-    return {
-        publicClient,
-        owner,
-        otherAccount,
-        contract
-    }
+  const publicClient = await viem.getPublicClient();
+  const [owner, otherAccount] = await viem.getWalletClients();
+  const contract = await viem.deployContract("CommitAndReveal", []);
+  return {
+    publicClient,
+    owner,
+    otherAccount,
+    contract,
+  };
 }
 
 describe("CommitReveal Scheme", () => {
-    describe('When the contract is deployed', () => {
-        it("Has a question", async () => {
-            const { contract } = await loadFixture(deployContractFixture);
-            const question = await contract.read.question();
-            expect(hexToString(question, { size: 32 })).to.eql("What is 2 + 2?");
-        });
-    })
+  describe("When the contract is deployed", () => {
+    it("Has a question", async () => {
+      const { contract } = await loadFixture(deployContractFixture);
+      const question = await contract.read.question();
+      expect(hexToString(question, { size: 32 })).to.eql("What is 2 + 2?");
+    });
+  });
 });
