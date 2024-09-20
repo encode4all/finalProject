dee1203c4225f49831094cb611c6367f475dee78
Idiakose O. Sunday <idiakosesunday@gmail.com>
2024-09-20 21:09:27 +0100

    test basic nft functionality

--- hardhat/test/TestBasicNft.ts
+++ hardhat/test/TestBasicNft.ts
@@ -5,26 +5,49 @@ import { hexToString } from "viem";
 
 
 async function deployContractFixture(name: string, symbol: string) {
-    const publicClient = await viem.getPublicClient();
-    const [owner, otherAccount] = await viem.getWalletClients();
-    const contract = await viem.deployContract("BasicNft", [name, symbol]);
-    return {
-      publicClient,
-      owner,
-      otherAccount,
-      contract,
-    };
+  const publicClient = await viem.getPublicClient();
+  const [owner, ...otherAccounts] = await viem.getWalletClients();
+  const contract = await viem.deployContract("BasicNft", [name, symbol]);
+  return {
+    publicClient,
+    owner,
+    otherAccounts,
+    contract,
+  };
+}
+
+async function waitForTrxSuccess(hash: `0x${string}`) {
+  const publicClient = await viem.getPublicClient();
+
+  const trxReceipt = await publicClient.waitForTransactionReceipt({ hash });
+
+  if (trxReceipt.status !== 'success') {
+    throw new Error(`${hash} transaction not successful`);
   }
 
-  
-describe("BasicNFT scheme", () => {
-  describe("when deployed", () => {
-    it("has initiated defaults", async () => {
-      const nftName = "NFT_name";
-      const nftSymbol = "NFT";
-      const d = () => deployContractFixture(nftName, nftSymbol);
-      const { contract } = await loadFixture(d);
+  return trxReceipt;
+}
 
+
+describe("BasicNFT scheme", () => {
+
+
+  let owner: { account: any };
+  let contract: any;
+  describe("when deployed", () => {
+
+    const nftName = "NFT_name";
+    const nftSymbol = "NFT";
+
+    before(async ()  => {
+      const d = () => deployContractFixture(nftName, nftSymbol);
+
+      const contractFixture = await loadFixture(d);
+      contract = contractFixture.contract;
+      owner = contractFixture.owner;
+    });
+
+    it("has initiated defaults", async () => {
       const [name, symbol] = await Promise.all([
         contract.read.name(),
         contract.read.symbol()
@@ -33,8 +56,37 @@ describe("BasicNFT scheme", () => {
       expect(name).to.eql(nftName);
       expect(symbol).to.eql(nftSymbol);
     });
+  });
+
+  describe("flows & usecases", () => {
+    const nftName = "NFT_name";
+    const nftSymbol = "NFT";
+    const tokenUri = "ipfs://foobar";
+    before(async ()  => {
+      const d = () => deployContractFixture(nftName, nftSymbol);
+
+      const contractFixture = await loadFixture(d);
+      contract = contractFixture.contract;
+      owner = contractFixture.owner;
+    });
 
     // it can mint
+    it('can mint nft', async () => {
+      const mintTx = await contract.write.mintNft([tokenUri], {
+        account: owner.account,
+      });
+
+      await waitForTrxSuccess(mintTx);
+
+      const t_counter = await contract.read.getTokenCounter();
+      expect(t_counter).to.eql(1n);
+
+    });
     // can get back uniqye tokenUri
+    it('can get tokenUri', async () => {
+
+      const tokenUriFromNft = await contract.read.tokenURI([0n]);
+      expect(tokenUriFromNft).to.eql(tokenUri);
+    });
   });
 });
