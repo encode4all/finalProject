edf07447d252b1dbf961e24328a84b984117a2c0
Idiakose O. Sunday <idiakosesunday@gmail.com>
2024-09-24 17:44:09 +0100

    Delete hardhat/contracts/Iverifier.sol

--- hardhat/contracts/Iverifier.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.24;
-
-interface IVerifier {
-    function checkSecret(string calldata) external view returns (bool);
-    function checkSecretByHash(bytes calldata) external view returns (bool);
-}
