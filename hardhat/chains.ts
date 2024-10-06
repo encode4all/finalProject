d436df51c077ec4df250ee527209a68f2a915fe6
Idiakose O. Sunday <idiakosesunday@gmail.com>
2024-10-06 23:19:27 +0100

    chore: lint fixes

--- hardhat/chains.ts
+++ hardhat/chains.ts
@@ -1,30 +1,30 @@
-import { defineChain } from 'viem';
+import { defineChain } from "viem";
 
 export const neoXTestnet = defineChain({
   id: 0xba9304,
-  name: 'Neo X Testnet',
+  name: "Neo X Testnet",
   nativeCurrency: {
     decimals: 18,
-    name: 'Neo GAS',
-    symbol: 'GAS',
+    name: "Neo GAS",
+    symbol: "GAS",
   },
   rpcUrls: {
-    default: { http: ['https://neoxt4seed1.ngd.network/'] },
+    default: { http: ["https://neoxt4seed1.ngd.network/"] },
   },
   blockExplorers: {
     default: {
-      name: 'NeoXScan',
+      name: "NeoXScan",
       url: 'https://xt4scan.ngd.network/"',
-      apiUrl: 'https://xt4scan.ngd.network/api',
+      apiUrl: "https://xt4scan.ngd.network/api",
     },
   },
   testnet: true,
   contracts: {
     multicall3: {
-      address: '0x82096F92248dF7afDdef72E545F06e5be0cf0F99',
+      address: "0x82096F92248dF7afDdef72E545F06e5be0cf0F99",
     },
     wgas10: {
-      address: '0x1CE16390FD09040486221e912B87551E4e44Ab17',
-    }
+      address: "0x1CE16390FD09040486221e912B87551E4e44Ab17",
+    },
   },
 });
--- hardhat/scripts/deploy_to_neox_with_ethers.ts
+++ hardhat/scripts/deploy_to_neox_with_ethers.ts
@@ -1,8 +1,8 @@
 import("@nomicfoundation/hardhat-ethers");
-import('@nomicfoundation/hardhat-toolbox');
+import("@nomicfoundation/hardhat-toolbox");
 import dotenv from "dotenv";
 import hre from "hardhat";
-dotenv.config({ path: "../../.env"  });
+dotenv.config({ path: "../../.env" });
 
 async function deployContract(contractName: string, contractDeployArgs: any[]) {
   const Contract = await hre.ethers.getContractFactory(contractName);
@@ -11,8 +11,12 @@ async function deployContract(contractName: string, contractDeployArgs: any[]) {
   const feeData = await hre.ethers.provider.getFeeData();
 
   // Calculate gas settings that are likely to be accepted
-  const maxFeePerGas = feeData.maxFeePerGas ? feeData.maxFeePerGas * BigInt(2) : null; // Double the suggested max fee
-  const maxPriorityFeePerGas = feeData.maxPriorityFeePerGas ? feeData.maxPriorityFeePerGas * BigInt(2) : null; // Double the suggested priority fee
+  const maxFeePerGas = feeData.maxFeePerGas
+    ? feeData.maxFeePerGas * BigInt(2)
+    : null; // Double the suggested max fee
+  const maxPriorityFeePerGas = feeData.maxPriorityFeePerGas
+    ? feeData.maxPriorityFeePerGas * BigInt(2)
+    : null; // Double the suggested priority fee
 
   // Deploy the contract with the calculated gas settings
   const contract = await Contract.deploy(...contractDeployArgs, {
@@ -26,16 +30,18 @@ async function deployContract(contractName: string, contractDeployArgs: any[]) {
 }
 
 async function main() {
-const aContract = await deployContract("CommitAndReveal", ["What are the secret words?"]);
-const aContractAddress = await aContract.getAddress();
+  const aContract = await deployContract("CommitAndReveal", [
+    "What are the secret words?",
+  ]);
+  const aContractAddress = await aContract.getAddress();
 
-const nftContract = await deployContract("BasicOnChainNft", [
-  "Group4 NFT",
-  "G04_NFT",
-  "This NFT can only be minted once",
-  "https://gateway.pinata.cloud/ipfs/bafybeidcwecintmlzlxsjs3gea63itszr56lgmix4qfjs3dwe4plsad3wq",
-aContractAddress,
-]);
+  const nftContract = await deployContract("BasicOnChainNft", [
+    "Group4 NFT",
+    "G04_NFT",
+    "This NFT can only be minted once",
+    "https://gateway.pinata.cloud/ipfs/bafybeidcwecintmlzlxsjs3gea63itszr56lgmix4qfjs3dwe4plsad3wq",
+    aContractAddress,
+  ]);
 }
 
 // Execute the main function
@@ -45,4 +51,3 @@ main()
     console.error(error);
     process.exit(1);
   });
-  
